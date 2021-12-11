c9fd274820ea2d9615100e30dfa2016c6a607248
Philip Sada <sadaphillip@gmail.com>
2021-12-11 19:47:14 +0100

    implemented design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import { Component } from "react";
 import './App.css';
+import WidgetBox from './components/WidgetBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      products:[]
+    };
+  }
+
+  getProducts(){
+    fetch('https://getgreenspark.mocklab.io/products')
+    .then((response)=>{
+      return response.json()
+    })
+    .then((data)=>{
+
+      //add additional data to the objects fetched
+      const publicProfileElements = {
+        headerClass:'widget-header public-profile__widget-header',
+        headerTextOne:'This product collects',
+        headerTextTwo:'100 plastic bottles',
+        headerTextOneClass:'widget-header-text-one public-profile__widget-header-text-one',
+        headerTextTwoClass:'widget-header-text-two public-profile__widget-header-text-two',
+        checkboxId:'public-profile__check-box',
+        badgeColours:[
+          {id:1,checkboxId:'public-profile__check-box--blue', labelClass:'badge-colour-box badge-colour-box--blue', fillClass:'badge-colour-fill badge-colour-fill--blue'},
+          {id:2,checkboxId:'public-profile__check-box--green', labelClass:'badge-colour-box badge-colour-box--green', fillClass:'badge-colour-fill badge-colour-fill--green'},
+          {id:3,checkboxId:'public-profile__check-box--beige', labelClass:'badge-colour-box badge-colour-box--beige', fillClass:'badge-colour-fill badge-colour-fill--beige'},
+          {id:4,checkboxId:'public-profile__check-box--white', labelClass:'badge-colour-box badge-colour-box--white', fillClass:'badge-colour-fill badge-colour-fill--white'},
+          {id:5,checkboxId:'public-profile__check-box--black', labelClass:'badge-colour-box badge-colour-box--black', fillClass:'badge-colour-fill badge-colour-fill--black'}
+       ],
+       activateBadgeCheckboxId:'public-profile__badge-toggle'
+      }
+      const treesElements = {
+        headerClass:'widget-header trees__widget-header',
+        headerTextOne:'This product plants',
+        headerTextTwo:'100 trees',
+        headerTextOneClass:'widget-header-text-one trees__widget-header-text-one',
+        headerTextTwoClass:'widget-header-text-two trees__widget-header-text-two',
+        checkboxId:'trees__check-box',
+        badgeColours:[
+          {id:1,checkboxId:'trees__check-box--blue', labelClass:'badge-colour-box badge-colour-box--blue', fillClass:'badge-colour-fill badge-colour-fill--blue'},
+          {id:2,checkboxId:'trees__check-box--green', labelClass:'badge-colour-box badge-colour-box--green', fillClass:'badge-colour-fill badge-colour-fill--green'},
+          {id:3,checkboxId:'trees__check-box--beige', labelClass:'badge-colour-box badge-colour-box--beige', fillClass:'badge-colour-fill badge-colour-fill--beige'},
+          {id:4,checkboxId:'trees__check-box--white', labelClass:'badge-colour-box badge-colour-box--white', fillClass:'badge-colour-fill badge-colour-fill--white'},
+          {id:5,checkboxId:'trees__check-box--black', labelClass:'badge-colour-box badge-colour-box--black', fillClass:'badge-colour-fill badge-colour-fill--black'}
+       ],
+       activateBadgeCheckboxId:'trees__badge-toggle'
+      }
+      const carbonElements = {
+        headerClass:'widget-header carbon__widget-header',
+        headerTextOne:'This product offsets',
+        headerTextTwo:'100kgs of carbon',
+        headerTextOneClass:'widget-header-text-one carbon__widget-header-text-one',
+        headerTextTwoClass:'widget-header-text-two carbon__widget-header-text-two',
+        checkboxId:'carbon__check-box',
+        badgeColours:[
+          {id:1, checkboxId:'carbon__check-box--blue', labelClass:'badge-colour-box badge-colour-box--blue', fillClass:'badge-colour-fill badge-colour-fill--blue'},
+          {id:2, checkboxId:'carbon__check-box--green', labelClass:'badge-colour-box badge-colour-box--green', fillClass:'badge-colour-fill badge-colour-fill--green'},
+          {id:3, checkboxId:'carbon__check-box--beige', labelClass:'badge-colour-box badge-colour-box--beige', fillClass:'badge-colour-fill badge-colour-fill--beige'},
+          {id:4, checkboxId:'carbon__check-box--white', labelClass:'badge-colour-box badge-colour-box--white', fillClass:'badge-colour-fill badge-colour-fill--white'},
+          {id:5, checkboxId:'carbon__check-box--black', labelClass:'badge-colour-box badge-colour-box--black', fillClass:'badge-colour-fill badge-colour-fill--black'}
+       ],
+       activateBadgeCheckboxId:'carbon__badge-toggle'
+      }
+
+      Object.assign(data[0], publicProfileElements);
+      Object.assign(data[1], treesElements);
+      Object.assign(data[2], carbonElements);
+
+      this.setState({products:data})
+    })
+    .catch((err)=>{
+      console.log(err)
+    });
+  }
+
+  componentDidMount(){
+    this.getProducts();
+  }
+
+  render(){
+    return(
+      <div className="app">
+          <div className='widget-card'>
+            <div className='widget-card-title'>
+                <h3>Per product widgets</h3>
+            </div>
+            <div className='line'></div>
+            <div className='widget-box-container'>
+                  <WidgetBox products={this.state.products}/>
+            </div>          
+         </div>
     </div>
-  );
+    )
+  }
+  
 }
 
-export default App;
+
---
+++ src/components/WidgetBox.js
@@ -0,0 +1,90 @@
+import React from "react";
+import vector from '../images/public-profile.svg';
+import carbonVector from '../images/carbon.svg';
+import greensparkText from '../images/greenspark-text.svg';
+import carbonGreensparkText from '../images/carbon-greenspark-text.svg';
+import {MdInfoOutline} from 'react-icons/md';
+import ReactTooltip from 'react-tooltip';
+
+export default function WidgetBox(props){
+    
+
+  
+    return (
+        <>
+        {
+            props.products.map((product)=>{
+                return(
+                  <div className='widget-box' key={product.id}>
+                    <div className={product.headerClass}>
+                      <div className='widget-header-details'>
+                         <div className='widget-img-container'>
+                             <img src={product.type === 'carbon' ? carbonVector : vector} className='vector'/>
+                             <img src={product.type === 'carbon' ? carbonGreensparkText:greensparkText} className='greenspark-text'/>
+                         </div>
+                        
+                         <div className='widget-header-text-container'>
+                           <p className={product.headerTextOneClass}>{product.headerTextOne}</p>
+                           <p className={product.headerTextTwoClass}>{product.headerTextTwo}</p>
+                         </div>
+                      </div>
+                    </div>
+                    <div className='widget-body'>
+                      <div className='widget-body-details'>
+                           <p className='widget-body-text link-to-public-profile'> Link to Public Profile</p>
+                            <div className='info-icon-container'> <a data-for='info' data-tip><MdInfoOutline className='info-icon'/></a></div>
+                            <ReactTooltip 
+                               id='info'
+                               effect='solid'
+                               delayHide={300}
+                               delayShow={300}
+                               delayUpdate={300}
+                               place={'left'}
+                               className='info-tool-tip-container'
+                             >
+                 
+                                 <p>This widget links directly to your public profile so that you can easily share your impact with your customers. 
+                                     Turn it off here if you do not want the badge to link to it.
+                                 </p>
+                                 <a href='/'>View Public Profile</a>
+               
+                           </ReactTooltip>
+                            <div className='check-box-container'>
+                                <input type="checkbox" id={product.checkboxId}/>
+                                <label for={product.checkboxId}></label>
+                                <div className='check-box-overlay'></div>
+                            </div>
+                      </div>
+                      <div className='widget-body-details'>
+                           <p className='widget-body-text badge-colour'>Badge colour</p>
+                           <div className='badge-colour-box-container'>
+                               {
+                                   product.badgeColours.map((badgeColour)=>{
+                                       return(
+                                        <label key={badgeColour.id} className={badgeColour.labelClass} for={badgeColour.checkboxId}>
+                                            <input type="checkbox" id={badgeColour.checkboxId}/>
+                                            <div className={badgeColour.fillClass}></div>
+                                        </label>
+                                       )
+                                   })
+                               }
+                              
+                           </div>
+                      </div>
+                      <div className='widget-body-details'>
+                           <p className='widget-body-text activate-bage'>Activate badge</p>
+                           <label className='toggle' for={product.activateBadgeCheckboxId}>
+                                   <input className='badge-toggle-input' type='checkbox' id={product.activateBadgeCheckboxId}/>
+                                   <div className='badge-toggle-fill'></div>
+                           </label>
+                      </div>
+                    </div>
+                    </div> 
+                )
+            })
+        }
+
+        </>
+    )
+
+}
